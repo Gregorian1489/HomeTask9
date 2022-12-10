7fe991717fb404190c6d2ebe6fe657e499fc5a87
Gregorian1489 <santijago@mail.ru>
2022-12-10 16:10:03 +0400

    add ht64

---
+++ ht64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void Numbers (int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    Console.Write($"{num} ");
+    Numbers(num - 1);
+}
+Console.WriteLine("Введите число:");
+int nums = Convert.ToInt32(Console.ReadLine());
+Numbers(nums);
