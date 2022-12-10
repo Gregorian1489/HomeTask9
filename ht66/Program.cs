80df5248e36514c60b713a7287045ccc53d02c1b
Gregorian1489 <santijago@mail.ru>
2022-12-10 17:13:50 +0400

    add ht66

---
+++ ht66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int sum = 0;
+void RecurSum (int num, int num1)
+{
+    if (num > num1) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (num++);
+    RecurSum(num,num1);
+
+}
+
+Console.WriteLine("Введите число M:");
+int nums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int nums1 = Convert.ToInt32(Console.ReadLine());
+
+RecurSum(nums,nums1);
